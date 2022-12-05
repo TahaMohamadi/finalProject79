71fa5101a6328320458e77b0d1a8e57c9980406e
tahamohammadi <taha.mohammadi@shahed.ac.ir>
2022-12-05 10:20:25 +0330

    first

--- src/main/java/ir/mohammadi/Main.java
+++ src/main/java/ir/mohammadi/Main.java
@@ -1,4 +1,6 @@
-package ir.mohammadi;public class Main {
+package ir.mohammadi;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
